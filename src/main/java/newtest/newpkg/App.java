5ace3063c6a918eb8ae38293604992fe9b932e50
bckumar2080 <40674604+bckumar2080@users.noreply.github.com>
2019-01-25 14:23:39 -0500

    sd

--- src/main/java/newtest/newpkg/App.java
+++ src/main/java/newtest/newpkg/App.java
@@ -9,10 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-                Object json = null;
-        ObjectMapper mapper1 = new ObjectMapper();
-	     ObjectWriter writer = mapper1.writer(new DefaultPrettyPrinter());
-	     writer.writeValue(new File("C:\\Applications\\jenkins\\workspace\\newApi.txt"),json);
+                
         System.out.println(json);
     }
 }
